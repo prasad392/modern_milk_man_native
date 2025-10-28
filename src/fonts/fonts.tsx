8f680b73daad1a9dad6ca60766e9199a7c54f146
prasad392 <venkata.prasad@gsstech.in>
2025-10-28 18:26:31 +0530

    react native 01

---
+++ src/fonts/fonts.tsx
@@ -0,0 +1,12 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+const Fonts = () => {
+  return (
+    <View>
+      <Text>Fonts</Text>
+    </View>
+  )
+}
+
+export default Fonts
